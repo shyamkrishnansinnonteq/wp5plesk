0f81a6d18900d4f8b566a74f84d763e2ed1b37d6
shyamtarams <shyamkrishnanpv535@gmail.com>
2021-11-13 11:31:44 +0530

    msg change

--- wp-admin/includes/schema.php
+++ wp-admin/includes/schema.php
@@ -402,7 +402,7 @@ function populate_options( array $options = array() ) {
 		'home'                            => $guessurl,
 		'blogname'                        => __( 'My Site' ),
 		/* translators: Site tagline. */
-		'blogdescription'                 => __( 'Just another WordPress sites' ),
+		'blogdescription'                 => __( 'Just another WordPress site' ),
 		'users_can_register'              => 0,
 		'admin_email'                     => 'you@example.com',
 		/* translators: Default start of the week. 0 = Sunday, 1 = Monday. */
